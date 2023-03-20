2c0677fb682aeefd13697fa1eb923a0f611b36a9
Eduardo Barreto <edugobarreto@gmail.com>
2023-03-19 22:56:03 -0300

    <REFACTOR> Changed Database Settings for Heroku

--- currency_rates/settings.py
+++ currency_rates/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=e!nho+tti+)=485%lq+hivrh_p0xl$4aizgitx$#%#i3dv-_4'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -81,16 +81,17 @@ WSGI_APPLICATION = 'currency_rates.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'x[KVC!>|l0aKM[5',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'x[KVC!>|l0aKM[5',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+DATABASE_URL = os.environ['DATABASE_URL']
 
 
 # Password validation
